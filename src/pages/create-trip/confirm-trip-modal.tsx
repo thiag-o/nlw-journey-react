69232a02f65b39c8b027da52395d7e1c0d18f136
Thiago Borges Mansano <thiagobmansano@gmail.com>
2024-07-12 17:02:18 -0300

    feat: adicionar a pagina trips

---
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -0,0 +1,56 @@
+import { User, X } from 'lucide-react';
+import { FormEvent } from 'react';
+import { Button } from '../../components/button';
+
+interface ConfirmTripModalProps {
+  closeConfirmTripModal: () => void;
+  createTrip: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+export function ConfirmTripModal({ closeConfirmTripModal, createTrip }: ConfirmTripModalProps) {
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="text-lg font-semibold">Confirmar criação da viagem</h2>
+            <button onClick={closeConfirmTripModal} className="size-5 text-zinc-400">
+              {' '}
+              <X />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Para concluir a criação da viagem para{' '}
+            <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de{' '}
+            <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha
+            seus dados abaixo:
+          </p>
+        </div>
+
+        <form className="space-y-3" onSubmit={createTrip}>
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <User className="text-zinc-400 size-5" />
+            <input
+              name="name"
+              placeholder="Seu nome completo"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <User className="text-zinc-400 size-5" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Seu email pessoal"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <Button type="submit" variant="primary" size="full">
+            Confirmar criação de viagem
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
